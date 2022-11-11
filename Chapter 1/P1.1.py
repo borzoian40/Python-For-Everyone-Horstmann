42b524bfcfd8e18ce8e1010e77a42a2022eb2b74
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-11 22:06:01 +0100

    Create P1.1.py

---
+++ Chapter 1/P1.1.py
@@ -0,0 +1,6 @@
+#Create a new function called greeting
+def greeting():
+    return "Welcome!! Wishing you the best things in every second of your life!"
+
+#Print the given function
+print(greeting())
