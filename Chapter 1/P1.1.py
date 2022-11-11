996ebdda82a8ad14f9ea80df3eac1f07bd71fa52
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-11 22:12:26 +0100

    Update P1.1.py

--- Chapter 1/P1.1.py
+++ Chapter 1/P1.1.py
@@ -1,6 +1,15 @@
+#First Approach
 #Create a new function called greeting
 def greeting():
     return "Welcome!! Wishing you the best things in every second of your life!"
 
 #Print the given function
 print(greeting())
+
+#Second Approach:
+def main():
+    greeting = "Hello there!!!"
+    print(greeting)
+
+if __name__ == "__main__":
+    main()
