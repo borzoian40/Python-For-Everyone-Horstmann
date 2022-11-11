4c7a77893a9933ff8d381e1da60b45ced15ba6ee
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-11 23:06:59 +0100

    Update P1.3.py

--- Chapter 1/P1.3.py
+++ Chapter 1/P1.3.py
@@ -4,7 +4,8 @@
 #And then intialize a variable called "final_ans" which is equal to 1.
 #In the for loop we have to start from number 1 to number+1. The reason we start from 1 is because that in Python the index starts from 0.
 #So if we start from 0 our final answer would always be zero. Hence we start from 1. 
-#And the reason we end with "number + 1" in our range is that because we want the final number of our range to be included as well hence we add one to the index for it to be included as well.
+#And the reason we end with "number + 1" in our range is that because we want the final number of our range to be included as well hence we add\
+#\one to the index for it to be included as well.
 
 #First approach
 def product(number):
