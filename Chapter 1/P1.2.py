32ff94ee74bec358b3d69bcace9003541b645253
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-11 22:26:25 +0100

    Create P1.2.py

---
+++ Chapter 1/P1.2.py
@@ -0,0 +1,25 @@
+#P1.2: Write a program that prints the sum of the first ten positive integers, 1 + 2 + … + 10.
+
+#At first we define a function called sum.
+#In the next step we assign a variable "ans" 
+#We then create a for loop range from 0 up until the number given by the user, which in this case is 10.
+def sum(num):
+    ans = 0
+    for i in range(num):
+        ans += i
+
+    return ans
+ 
+print(sum(10))
+
+
+#If we didn't have an inital input:
+def sum(num):
+    ans = 0
+    for i in range(num):
+        ans += i
+
+    print(ans)
+
+number = int(input())
+sum(number)
