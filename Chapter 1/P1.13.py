e53ee1418880f364c847c26c16bfac57678296e6
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-15 12:50:52 +0100

    Update P1.13.py

--- Chapter 1/P1.13.py
+++ Chapter 1/P1.13.py
@@ -1,10 +1,11 @@
 # P1.13 Wrtie a program that prints the United States flag, using * and = characters.
 
 #We define two for loops; the first one for "rows" and the second one for "columns".
-#
+#Now for the stars, the program prints them only when the sum of indices of rows and columns are even, otherwise it prints a "space"(i.e. " ") 
+#and moves on to the next index.
+#at the end of the first for loop we use print() so that the program goes to the next line and prints the rest of the remaining elements of our U.S. flag.
 
-
-
+#REMEMEBR: THE AMERICAN FLAG HAS 50 STARS EACH OF THEM REPRESENTING A STATE.
 
 def main():
     for i in range(0, 15):
@@ -22,3 +23,10 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
+
+
+    
+    
+#P.S. The idea and algorithm of the above code was taken from this website: https://petamind.com/fun-coding-challenge-print-the-american-flag/
+
