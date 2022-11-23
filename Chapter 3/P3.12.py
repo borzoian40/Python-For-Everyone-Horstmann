3b92cdb94120dc922451ca5605442068de5db858
Borzu <74207166+borzoian40@users.noreply.github.com>
2022-11-23 13:18:42 +0100

    Update P3.12.py

--- Chapter 3/P3.12.py
+++ Chapter 3/P3.12.py
@@ -40,3 +40,32 @@ def main(grade):
 if __name__ == "__main__":
     result = (input("Please enter your grade: "))
     print(f"The numeric value is: {main(result)}")
+    
+    
+#### Second Approach ####
+
+grade = input("Please enter your grade: ")
+grade = grade.upper()
+
+if grade[0] == "A":
+    score = 4.0
+elif grade[0] == "B":
+    score = 3.0
+elif grade[0] == "C":
+    score = 2.0
+elif grade[0] == "D":
+    score = 1.0
+else:
+    score = 0.0
+
+
+if len(grade) > 1 and grade[0] != "F":
+    if grade[1] == "+":
+        score += 0.3
+    elif grade[1] == "-":
+        score -= 0.3
+else:
+    print("Unfortunately, you have to redo the test.")
+
+#Final score
+print(f"The numeric value of your grade is: {score}")
