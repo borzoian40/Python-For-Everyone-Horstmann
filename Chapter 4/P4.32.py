d12146b4c73758fb4deb49033492a6d575b7b2ba
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-09 21:33:45 +0200

    Create P4.32.py

---
+++ Chapter 4/P4.32.py
@@ -0,0 +1,8 @@
+"""
+•• Business P4.32 Your company has shares of stock it would like to sell when their value exceeds a 
+certain target price. Write a program that reads the target price and then reads the 
+current stock price until it is at least the target price. Your program should read
+a sequence of floating-point values from standard input. Once the minimum is 
+reached, the program should report that the stock price exceeds the target price.
+
+"""
