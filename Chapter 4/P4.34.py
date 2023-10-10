fd30df5a65ff63f3cf7631a8ada08b11cdb2915e
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-10 10:35:54 +0200

    Create P4.34.py

---
+++ Chapter 4/P4.34.py
@@ -0,0 +1,9 @@
+"""
+•• Business P4.34 You need to control the number of people who can be in an oyster bar at the same
+time. Groups of people can always leave the bar, but a group cannot enter the bar 
+if they would make the number of people in the bar exceed the maximum of 100 
+occupants. Write a program that readsthe sizes of the groupsthat arrive or depart.
+Use negative numbers for departures. After each input, display the current number 
+of occupants. As soon as the bar holds the maximum number of people, report that 
+the bar is full and exit the program
+"""
