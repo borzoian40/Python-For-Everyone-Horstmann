44f3cbd20650d5b508a72d816a8581378c92d4e9
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-10 14:33:48 +0200

    Update P4.35_RAW.py

--- Chapter 4/P4.35.py
+++ Chapter 4/P4.35.py
@@ -15,3 +15,50 @@ Write a program that implements this algorithm. The user should supply an 8-digi
 number, and you should print out whether the number is valid or not. If it is not 
 valid, you should print the value of the check digit that would make it valid.
 """
+#Raw file for P4.35. After spending almost 3 hours!!! 
+def main():
+    count = 0
+    while count <= 3:
+        sum1 = 0
+        value_input = input("Please enter your 8 digit credit card number: ")
+        value_input = value_input.replace(" ", "")
+        # Convert the input to a string
+        conversion = str(value_input)
+        # Use slicing to get every other digit starting from the first digit (index 0)
+        array_conversion1 = conversion[1::2]
+        array_conversion2 = conversion[::2]
+        list = []
+        for i in range(len(array_conversion1)):
+            # Convert each character back to an integer before adding to sum
+            sum1 += int(array_conversion1[i])
+
+        for j in range(len(array_conversion2)):
+            list.append(int(array_conversion2[j]) * 2)
+
+        string_conversion = " "
+
+        liststr = string_conversion.join([str(element) for element in list])
+        liststr = liststr.replace(" ", "")
+
+        sum2 = 0
+        addition_liststr = liststr[::]
+        for i in range(len(addition_liststr)):
+            sum2 += int(addition_liststr[i])
+
+        final_sum = sum1 + sum2
+
+        if final_sum % 10 == 0:
+            print("Your card is valid!")
+            break
+        else:
+            count += 1
+            if count == 3:
+                print("Your card is blocked!")
+                break
+
+            print("Your CARD IS NOT VALID! PLEASE TRY AGAIN!")
+
+
+if __name__ == "__main__":
+    main()
+
