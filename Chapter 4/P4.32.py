70843e5ddac183366a1af907523eff3890b3145e
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-10 09:51:49 +0200

    Update P4.32.py

--- Chapter 4/P4.32.py
+++ Chapter 4/P4.32.py
@@ -6,3 +6,19 @@ a sequence of floating-point values from standard input. Once the minimum is
 reached, the program should report that the stock price exceeds the target price.
 
 """
+def main():
+    # Read the target price from the user
+    target_price = float(input("Enter the target price: "))
+
+    # Loop to read and compare stock prices
+    while True:
+        current_price = float(input("Enter the current stock price: "))
+
+        if current_price >= target_price:
+            print("Stock price exceeds the target price.")
+            break
+
+
+if __name__ == "__main__":
+    main()
+    
