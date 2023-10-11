3f12c81c5cef9cf78554fdf5096ec6212ac2be84
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-11 07:33:34 +0200

    Create P5.1.py

---
+++ Chapter 5/P5.1.py
@@ -0,0 +1,8 @@
+"""
+ P5.1 Write the following functions and provide a program to test them.
+a. def smallest(x, y, z) (returning the smallest of the arguments)
+b. def average(x, y, z) (returning the average of the arguments)
+"""
+
+
+
