d84089f18bd510cf53c28ab1ca7ca45ea78cd526
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-11 07:45:04 +0200

    Create P5.2.py

---
+++ Chapter 5/P5.2.py
@@ -0,0 +1,7 @@
+"""
+ P5.2 Write the following functions and provide a program to test them.
+a. def allTheSame(x, y, z) (returning true if the arguments are all the same)
+b. def allDifferent(x, y, z) (returning true if the arguments are all different)
+c. def sorted(x, y, z) (returning true if the arguments are sorted, with the 
+smallest one coming first)
+"""
