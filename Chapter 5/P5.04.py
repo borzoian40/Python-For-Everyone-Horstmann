0ca85ff7f2d8b405b9be37187d9f79b0cb88e0c2
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-11 08:30:25 +0200

    Create P5.04.py

---
+++ Chapter 5/P5.04.py
@@ -0,0 +1,7 @@
+"""
+• P5.4 Write a function
+def middle(string)
+that returns a string containing the middle character in string if the length of string is 
+odd, or the two middle characters if the length is even. For example, middle("middle") 
+returns "dd".
+"""
