9d54f7d95d0712c62415590f4694324a29576a9b
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-11 07:44:09 +0200

    Update P5.1.py

--- Chapter 5/P5.1.py
+++ Chapter 5/P5.1.py
@@ -3,6 +3,29 @@
 a. def smallest(x, y, z) (returning the smallest of the arguments)
 b. def average(x, y, z) (returning the average of the arguments)
 """
+def main():
+    x = int(input("Please enter your first number: "))
+    y = int(input("Please enter your second number: "))
+    z = int(input("Please enter your third number: "))
 
+    min_val = smallest(x, y, z)
+    avg = average(x, y, z)
+    print(f"The smallest number is: {min_val}")
+    print(f"The average is: {avg}")
+
+    smallest(x, y, z)
+    average(x, y, z)
+
+
+def smallest(x, y, z):
+    return min(x, y, z)
+
+
+def average(x, y, z):
+    return (x + y + z) // 3
+
+
+if __name__ == "__main__":
+    main()
 
 
