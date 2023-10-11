3af250ceacea0cd7be57642d1095145e8763615a
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-11 08:31:00 +0200

    Create P5.05.py

---
+++ Chapter 5/P5.05.py
@@ -0,0 +1,6 @@
+"""
+• P5.5 Write a function
+def repeat(string, n, delim)
+that returns the string string repeated n times, separated by the string delim. For
+example, repeat("ho", 3, ", ") returns "ho, ho, ho".
+"""
