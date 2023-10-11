3a3246e5ebdf398bbcb806b0950cb68a33881aad
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-11 08:17:06 +0200

    Create P5.3.py

---
+++ Chapter 5/P5.3.py
@@ -0,0 +1,10 @@
+"""
+P5.3 Write the following functions and provide a program to test them.
+a. def firstDigit(n) (returning the first digit of the argument)
+b. def lastDigit(n) (returning the last digit of the argument)
+c. def digits(n) (returning the number of digits in the argument)
+For example, firstDigit(1729) is 1, lastDigit(1729) is 9, and digits(1729) is 4
+
+"""
+def main():
+  
