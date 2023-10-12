606f88ba64846cb473fa2311d7df6593ee2aba4c
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-12 20:08:33 +0200

    Update P5.04.py

--- Chapter 5/P5.04.py
+++ Chapter 5/P5.04.py
@@ -5,3 +5,21 @@ that returns a string containing the middle character in string if the length of
 odd, or the two middle characters if the length is even. For example, middle("middle") 
 returns "dd".
 """
+
+def main():
+  
+    stringlength = input("Please enter your string: ")
+    character = middle(stringlength)
+    print(character)
+
+
+def middle(string):
+    if len(string) % 2 != 0:
+        return string[(len(string))// 2]
+    else:
+        return string[len(string) // 2-1:(len(string) // 2) + 1]
+
+
+if __name__ == "__main__":
+    main()
+
