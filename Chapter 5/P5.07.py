5139ca324be2ce9084337b93cde85d33796600ca
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-14 10:31:11 +0200

    Update P5.07.py

--- Chapter 5/P5.07.py
+++ Chapter 5/P5.07.py
@@ -4,3 +4,22 @@ def countWords(string)
 that returns a count of all words in the string string. Words are separated by spaces. 
 For example, countWords("Mary had a little lamb") should return 5.
 """
+def main():
+    
+    user_input = input("Please enter your sentence:\n")
+    count = countWords(user_input)
+    print(f"Total number of words: {count}")
+
+
+def countWords(string):
+    count = 0
+
+    for char in string:
+        if char == " ":
+            count += 1
+
+    return count + 1
+
+
+if __name__ == "__main__":
+    main()
