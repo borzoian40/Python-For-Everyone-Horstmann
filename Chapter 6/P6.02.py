9bf4224d74fec0a9eed282055c59587e2b811014
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-14 16:45:42 +0200

    Update P6.02.py

--- Chapter 6/P6.02.py
+++ Chapter 6/P6.02.py
@@ -3,3 +3,23 @@
 contained in the list. When the list contains ten numbers, the program displays the 
 contents and quits.
 """
+def main():
+    numbers = []  # Initialize an empty list to store the numbers
+    
+    while len(numbers) < 10:  # Continue until the list contains ten numbers
+        try:
+            num = float(input("Enter a number: "))
+            
+            if num not in numbers:
+                numbers.append(num)
+            else:
+                print("Number already in the list.")
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+    print("List of numbers:")
+    for number in numbers:
+        print(number)
+
+if __name__ == "__main__":
+    main()
