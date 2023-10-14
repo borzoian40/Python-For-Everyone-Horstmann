df03e1b7c00bc85a31262fea3c496a4d9642405c
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-14 10:10:12 +0200

    Update P5.05.py

--- Chapter 5/P5.05.py
+++ Chapter 5/P5.05.py
@@ -4,12 +4,16 @@ def repeat(string, n, delim)
 that returns the string string repeated n times, separated by the string delim. For
 example, repeat("ho", 3, ", ") returns "ho, ho, ho".
 """
-
-def main():
-
 def repeat(string, n, delim):
-  return 
+    if n <= 0:
+        return ""  # If n is non-positive, return an empty string
+    repeated_string = (string + delim) * (n - 1) + string
+    return repeated_string
 
+# Example usage:
+input_string = "ho"
+repetitions = 3
+delimiter = ", "
+result = repeat(input_string, repetitions, delimiter)
+print(result)
 
-if __name__ == "__main__":
-  main()
