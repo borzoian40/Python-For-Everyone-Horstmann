5aa88d3342fc9e92443e33c8ac2201ba825f2bd2
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-14 10:11:08 +0200

    Create P5.07.py

---
+++ Chapter 5/P5.07.py
@@ -0,0 +1,6 @@
+"""
+•• P5.7 Write a function
+def countWords(string)
+that returns a count of all words in the string string. Words are separated by spaces. 
+For example, countWords("Mary had a little lamb") should return 5.
+"""
