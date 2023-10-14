ec861729ef86bfda25e08d25a3efb129fc63f7a7
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-14 10:32:31 +0200

    Update P5.08.py

--- Chapter 5/P5.08.py
+++ Chapter 5/P5.08.py
@@ -1,1 +1,10 @@
-
+"""
+P5.08
+•• P5.8 It is a well-known phenomenon that most people are easily able to read a text whose 
+words have two characters flipped, provided the first and last letter of each word are 
+not changed. For example,
+I dn’ot gvie a dman for a man taht can olny sepll a wrod one way. (Mrak Taiwn)
+Write a function scramble(word) that constructs a scrambled version of a given word,
+randomly flipping two characters other than the first and last one. Then write a 
+program that reads words and prints the scrambled words.
+"""
