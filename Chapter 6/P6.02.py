b78e40920779b69e5180261cfd272cebbfcd146a
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-14 16:20:10 +0200

    Update P6.02.py

--- Chapter 6/P6.02.py
+++ Chapter 6/P6.02.py
@@ -1,1 +1,5 @@
-
+"""
+• P6.2 Write a program that reads numbers and adds them to a list if they aren’t already 
+contained in the list. When the list contains ten numbers, the program displays the 
+contents and quits.
+"""
