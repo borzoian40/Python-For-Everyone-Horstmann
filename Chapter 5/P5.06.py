6a39aeccca8bf6d1b985b7d3cccd29148e62c375
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-14 09:58:12 +0200

    Update P5.06.py

--- Chapter 5/P5.06.py
+++ Chapter 5/P5.06.py
@@ -4,3 +4,21 @@ def countVowels(string)
 that returns a count of all vowels in the string. Vowels are the letters a, e, i, o, 
 and u, and their uppercase variants
 """
+def main():
+    user_string = input("Please enter your string: ")
+    vowel_count = countVowels(user_string)
+    print(f"The number of vowels in the string is: {vowel_count}")
+
+def countVowels(string):
+    count = 0
+    letters = ["a", "e", "i", "o", "u"]
+
+    new_string = string.lower()
+    for char in new_string:
+        if char in letters:
+            count += 1
+
+    return count
+
+if __name__ == "__main__":
+    main()
