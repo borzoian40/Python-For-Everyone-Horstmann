fa5ddfbf2efb84d80735df3bb8aef9debfb78aaa
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-15 15:09:26 +0200

    Update P6.02.py

--- Chapter 6/P6.02.py
+++ Chapter 6/P6.02.py
@@ -1,15 +1,10 @@
-"""
-• P6.2 Write a program that reads numbers and adds them to a list if they aren’t already 
-contained in the list. When the list contains ten numbers, the program displays the 
-contents and quits.
-"""
 def main():
     numbers = []  # Initialize an empty list to store the numbers
-    
+
     while len(numbers) < 10:  # Continue until the list contains ten numbers
         try:
             num = float(input("Enter a number: "))
-            
+
             if num not in numbers:
                 numbers.append(num)
             else:
@@ -17,9 +12,8 @@ def main():
         except ValueError:
             print("Invalid input. Please enter a valid number.")
 
-    print("List of numbers:")
-    for number in numbers:
-        print(number)
+    print(f"List of numbers: {numbers}")
+
 
 if __name__ == "__main__":
     main()
