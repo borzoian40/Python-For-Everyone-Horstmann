9997459c8532863aca1e1ffc337ed4f223ee1192
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-15 15:09:52 +0200

    Update P6.02.py

--- Chapter 6/P6.02.py
+++ Chapter 6/P6.02.py
@@ -1,3 +1,8 @@
+"""
+• P6.2 Write a program that reads numbers and adds them to a list if they aren’t already 
+contained in the list. When the list contains ten numbers, the program displays the 
+contents and quits.
+"""
 def main():
     numbers = []  # Initialize an empty list to store the numbers
 
