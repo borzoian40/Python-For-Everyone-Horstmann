9a3430f51adf7df9494788488e21d97f7a561fbd
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-15 15:11:28 +0200

    Create P6.03.py

---
+++ Chapter 6/P6.03.py
@@ -0,0 +1,5 @@
+"""
+•• P6.3 Write a program that adds all numbers from 2 to 10,000 to a list. Then remove the 
+multiples of 2 (but not 2), multiples of 3 (but not 3), and so on, up to the multiples of
+100. Print the remaining values.
+"""
