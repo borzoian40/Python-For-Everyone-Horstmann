75a25c7c0bdffe6fe8679b321ba43de549d08959
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-16 11:13:15 +0200

    Create P6.08.py

---
+++ Chapter 6/P6.08.py
@@ -0,0 +1,7 @@
+"""
+•• P6.8 Compute the alternating sum of all elements in a list. For example, if your program 
+reads the input
+then it computes
+1 4 9 16 9 7 4 9 11
+1 – 4 + 9 – 16 + 9 – 7 + 4 – 9 + 11 = –2
+"""
