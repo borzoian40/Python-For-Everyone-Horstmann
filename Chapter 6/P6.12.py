3440d186c44dfe8cd33bdd50463ba4bba2cafdec
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-18 19:08:26 +0200

    Create P6.12.py

---
+++ Chapter 6/P6.12.py
@@ -0,0 +1,8 @@
+"""
+•• P6.12 Write a function def sameSet(a, b) that checks whether two lists have the same elements in some order, ignoring duplicates. For example, the two lists
+1 4 9 16 9 7 4 9 11
+and
+11 11 7 9 16 4 1
+would be considered identical. You will probably need one or more helper 
+functions.
+"""
