54a10cc1bf45a5268663ab9b12451e6be745aefa
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-18 19:09:06 +0200

    Create P6.13.py

---
+++ Chapter 6/P6.13.py
@@ -0,0 +1,12 @@
+"""
+••• P6.13 Write a function def sameElements(a, b) that checks whether two lists have the same 
+elements in some order, with the same multiplicities. For example,
+1 4 9 16 9 7 4 9 11
+and
+11 1 4 9 16 9 7 4 9
+would be considered identical, but
+1 4 9 16 9 7 4 9 11
+and
+11 11 7 9 16 4 1 4 9
+would not. You will probably need one or more helper functions
+"""
