8fe2981b655b5c28d42a9f7ff385ef9626d43307
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-18 19:05:29 +0200

    Update P6.11.py

--- Chapter 6/P6.11.py
+++ Chapter 6/P6.11.py
@@ -20,10 +20,15 @@ def main():
         x2 = int(digits)
         list2.append(x2)
 
+    print(equals(list1, list2))
+
+
 def equals(lst1, lst2):
     length1 = len(lst1)
     length2 = len(lst2)
-
+    
+    """
+    The mistake that I made at first is make a true if statement return True.
     if length1 == length2:
         for i in range(length1):
             if lst1[i] == lst2[i]:
@@ -32,8 +37,13 @@ def equals(lst1, lst2):
                 return False
 
         return True
-    
-
+    """
+    if length1 != length2:
+        return False
+    for i in range(length1):
+        if lst1[i] != lst2[i]:
+            return False
+    return True
 
 if __name__ == "__main__":
     main()
