81039e9a55c1ea78d7f9a1e612bb16b133e97554
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-19 12:16:39 +0200

    Update P6.13.py

--- Chapter 6/P6.13.py
+++ Chapter 6/P6.13.py
@@ -10,3 +10,32 @@ and
 11 11 7 9 16 4 1 4 9
 would not. You will probably need one or more helper functions
 """
+def main():
+    numbers = input("Please enter values of list1:\n")
+    values = numbers.split()
+
+    list1 = []
+    for digits in values:
+        num = int(digits)
+        list1.append(num)
+
+    numbers2 = input("Please enter values of list2:\n")
+    values2 = numbers2.split()
+
+    list2 = []
+    for digits2 in values2:
+        num2 = int(digits2)
+        list2.append(num2)
+
+    print(sameSet(list1, list2))
+
+
+def sameSet(a, b):
+    if set(a) != set(b):
+        return False
+    else:
+        return True
+
+
+if __name__ == "__main__":
+    main()
