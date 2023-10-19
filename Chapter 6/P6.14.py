348c313669059a35186d828df50c7745057e835a
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-19 12:16:22 +0200

    Update P6.14.py

--- Chapter 6/P6.14.py
+++ Chapter 6/P6.14.py
@@ -2,32 +2,3 @@
 •• P6.14 Modify the program in Worked Example 6.2 to use randomly-generated numbers 
 for the die values instead of reading them from the user.
 """
-def main():
-    numbers = input("Please enter values of list1:\n")
-    values = numbers.split()
-
-    list1 = []
-    for digits in values:
-        num = int(digits)
-        list1.append(num)
-
-    numbers2 = input("Please enter values of list2:\n")
-    values2 = numbers2.split()
-
-    list2 = []
-    for digits2 in values2:
-        num2 = int(digits2)
-        list2.append(num2)
-
-    print(sameSet(list1, list2))
-
-
-def sameSet(a, b):
-    if set(a) != set(b):
-        return False
-    else:
-        return True
-
-
-if __name__ == "__main__":
-    main()
