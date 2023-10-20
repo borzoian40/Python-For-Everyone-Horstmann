52086104ff52494bfc5f4eff37bd920062ad145d
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-20 13:40:23 +0200

    Update and rename P16.6.py to P6.16.py

--- Chapter 6/P6.16.py
+++ Chapter 6/P6.16.py
@@ -4,3 +4,4 @@ list, and prints the die values, marking only the longest run, like this:
 1 2 5 5 3 1 2 4 3 (2 2 2 2) 3 6 5 5 6 3 1
 If there is more than one run of maximum length, mark the first one.
 """
+
