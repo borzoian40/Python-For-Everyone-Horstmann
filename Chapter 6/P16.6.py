e330d659c481464f6837130d2ea7b8fd3d3621d0
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-20 10:02:27 +0200

    Create P16.6.py

---
+++ Chapter 6/P16.6.py
@@ -0,0 +1,6 @@
+"""
+•• P6.16 Write a program that generates a sequence of 20 random die tosses, stores them in a 
+list, and prints the die values, marking only the longest run, like this:
+1 2 5 5 3 1 2 4 3 (2 2 2 2) 3 6 5 5 6 3 1
+If there is more than one run of maximum length, mark the first one.
+"""
