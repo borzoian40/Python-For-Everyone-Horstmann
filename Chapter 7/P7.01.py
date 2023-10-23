ff7fe9fcd0e2bbb94f7dd854c8d0b3dfbc274161
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-23 12:06:27 +0200

    Update P7.01.py

--- Chapter 7/P7.01.py
+++ Chapter 7/P7.01.py
@@ -6,3 +6,16 @@ Close the file.
 Open the same file again.
 Read the message into a string variable and print it.
 """
+FILENAME = 'P7.01.txt'
+
+def main():
+    with open(FILENAME, "w") as my_file:
+        my_file.write("Hello, World!")
+
+    with open(FILENAME, "r") as my_file:
+        message = my_file.read()
+
+    print(message)
+
+if __name__ == "__main__":
+    main()
