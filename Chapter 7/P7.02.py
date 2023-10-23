a3c02aa80f4d4e6c44da28e7df281a626ea9b224
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-23 12:11:37 +0200

    Create P7.02.py

---
+++ Chapter 7/P7.02.py
@@ -0,0 +1,15 @@
+"""
+• P7.2 Write a program that reads a file containing text. Read each line and send it to the 
+output file, preceded by line numbers. If the input file is
+Mary had a little lamb
+Whose fleece was white as snow. 
+And everywhere that Mary went, 
+The lamb was sure to go!
+then the program produces the output file
+/* 1 */ Mary had a little lamb
+/* 2 */ Whose fleece was white as snow.
+/* 3 */ And everywhere that Mary went,
+/* 4 */ The lamb was sure to go!
+Prompt the user for the input and output file 
+names
+"""
