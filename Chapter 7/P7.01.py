e4f590face0f434675e49b5760c07fd7aafdf86b
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-23 11:18:24 +0200

    Create P7.01.py

---
+++ Chapter 7/P7.01.py
@@ -0,0 +1,8 @@
+"""
+ P7.1 Write a program that carries out the following tasks:
+Open a filewith the name hello.txt.
+Storethemessage“Hello,World!”inthefile. 
+Close the file.
+Openthe same file again.
+Readthemessage into a string variable andprintit.
+"""
