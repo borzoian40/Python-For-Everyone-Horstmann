0bdc5e9ff1b5c216fe9ae14f93824b6987439787
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-24 14:04:23 +0200

    Create P7.09.py

---
+++ Chapter 7/P7.09.py
@@ -0,0 +1,15 @@
+"""
+•• P7.9 Write a program that reads each line in a file, reversesits lines, and writesthem to 
+another file. For example, if the file input.txt contains the lines
+Mary had a little lamb
+Its fleece was white as snow 
+And everywhere that Mary went 
+The lamb was sure to go.
+and you run
+reverse input.txt output.txt
+then output.txt contains
+The lamb was sure to go.
+And everywhere that Mary went 
+Its fleece was white as snow 
+Mary had a little lamb
+"""
