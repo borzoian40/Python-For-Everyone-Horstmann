72b006552ab835a2f4a656e5a81ccbe2adcd19fc
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-24 14:03:42 +0200

    Create P7.03.py

---
+++ Chapter 7/P7.03.py
@@ -0,0 +1,4 @@
+"""
+• P7.3 Repeat Exercise • P7.2, but allow the user to specify the file name on the command 
+line. If the user doesn’t specify any file name, then prompt the user for the name.
+"""
