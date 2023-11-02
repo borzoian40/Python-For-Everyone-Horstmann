2a92b19aa6e107f5693d044c0dc90c384e5004e4
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-11-02 12:45:53 +0100

    REDOOOO

---
+++ Chapter 8/P8.02.py
@@ -0,0 +1,31 @@
+"""
+• P8.2 Write a program that counts how often each word occurs in a text file.
+"""
+def count_word_occurrences(file_name):
+    word_count = {}
+    try:
+        with open(file_name, 'r') as file:
+            for line in file:
+                words = line.split()
+                for word in words:
+                    word = word.strip('.,!?\'"(){}[]').lower()
+                    if word not in word_count:
+                        word_count[word] = 1
+                    else:
+                        word_count[word] += 1
+    except FileNotFoundError:
+        print("File not found.")
+        return word_count
+
+    return word_count
+
+def main():
+    file_name = 'babynames.txt'  # Replace with your file name or path
+    word_count = count_word_occurrences(file_name)
+
+    if word_count:
+        for word, count in word_count.items():
+            print(f'Word: {word}, Count: {count}')
+
+if __name__ == "__main__":
+    main()
