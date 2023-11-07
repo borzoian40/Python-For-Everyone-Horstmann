70e06a8622b17a01ea561148ec526a088756c50a
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-11-07 13:42:15 +0100

    Update P1.05.java

--- Chapter 1/P1.05.java
+++ Chapter 1/P1.05.java
@@ -1,3 +1,10 @@
+"""
+P1.5 Write a program that displays your name inside a box on the screen, like this:
+          +------+
+          | Dave |
+          +------+
+    Do your best to approximate lines with characters such as | - +.
+"""
 import java.util.Scanner;
 
 public class hello {
