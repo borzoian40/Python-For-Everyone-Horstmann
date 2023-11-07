3be72f0577f42e8fd7f044710396adcabb58f3e4
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-11-07 13:52:47 +0100

    Create P1.05.py

---
+++ Chapter 1/P1.05.py
@@ -0,0 +1,9 @@
+"""
+/*
+P1.5 Write a program that displays your name inside a box on the screen, like this:
+          +------+
+          | Dave |
+          +------+
+    Do your best to approximate lines with characters such as | - +.
+*/
+"""
