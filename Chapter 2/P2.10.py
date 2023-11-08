661e0994ef6098513cc18f0b7a86ecef0c8ba4cc
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-11-08 12:47:07 +0100

    Create P2.10.py

---
+++ Chapter 2/P2.10.py
@@ -0,0 +1,17 @@
+"""
+••• P2.10 Write a program that helps a person decide whether to buy a hybrid car. Your 
+program’s inputs should be:
+• The cost of a new car
+• The estimated miles driven per year
+• The estimated gas price
+• The efficiency in miles per gallon
+• The estimated resale value after five years
+Compute the total cost of owning the car for 
+five years. (For simplicity, we will not take the 
+cost of financing into account.) Obtain realistic
+prices for a new and used hybrid and a com- © asiseeit/iStockphoto.
+parable car from the Web. Run your program twice, using today’s gas price and 
+15,000 miles per year. Include pseudocode and the program runs with your 
+assignment.
+
+"""
