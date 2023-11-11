c12cc7fa2a4eec9091ba468c282630b9ae9caa8f
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-11-12 00:32:00 +0100

    Create P3.17.py

---
+++ Chapter 3/P3.17.py
@@ -0,0 +1,10 @@
+"""
+P3.17 Write a program that reads in a string and prints whether it
+• contains only letters.
+• contains only uppercase letters.
+• contains only lowercase letters.
+• contains only digits.
+• contains only letters and digits.
+• starts with an uppercase letter.
+• ends with a period.
+"""
