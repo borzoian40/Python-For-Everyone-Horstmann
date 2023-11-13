6e6a620c935c02625149cfe057c63c91a3e298ff
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-11-13 19:41:30 +0100

    P3.30.py

---
+++ Chapter 3/P3.30.py
@@ -0,0 +1,15 @@
+"""
+••• P3.30 French country names are feminine when they end with the letter e, masculine otherwise, except for the following which are masculine even though they end with e:
+• le Belize
+• le Cambodge
+• le Mexique
+• le Mozambique
+• le Zaïre
+• le Zimbabwe
+Write a program that readsthe French name of a country and addsthe article: le for 
+masculine or la for feminine, such as le Canada or la Belgique.
+However, if the country name starts with a vowel, use l’; for example, l’Afghanistan. 
+For the following plural country names, use les:
+• les Etats-Unis
+• les Pays-Bas
+"""
