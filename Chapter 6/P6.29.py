22578ed6730f2d7cf3844d6b28eeff16a46d4c0b
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-11-16 14:06:27 +0100

    P6.29.py

---
+++ Chapter 6/P6.29.py
@@ -0,0 +1,9 @@
+"""
+ P6.29 Write a function def appendList(a, b) that appends one list 
+after another. For example, if a is
+1 4 9 16
+and b is
+9 7 4 9 11
+then appendList returns a new list containing the values
+1 4 9 16 9 7 4 9 11
+"""
