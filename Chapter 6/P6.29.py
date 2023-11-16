9dae131fd92ad709d4032c817feb7d8a76bf5203
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-11-16 14:17:53 +0100

    P6.29.py
    Append List

--- Chapter 6/P6.29.py
+++ Chapter 6/P6.29.py
@@ -7,3 +7,34 @@ and b is
 then appendList returns a new list containing the values
 1 4 9 16 9 7 4 9 11
 """
+def appendList(a, b):
+    final_list = a + b
+    return final_list
+
+def main():
+    # input the numbers with spaces included
+    input_str = input("Please enter your numbers:\n")
+    numbers = input_str.split()  # add them to a list
+    # introduce an empty list
+    list_numbers = []
+    # now add those numbers as int to "list_numbers"
+    for num in numbers:
+        x = int(num)
+        list_numbers.append(x)
+
+    # Do the same process for the second list
+    input_str2 = input("Please enter your numbers:\n")
+    numbers2 = input_str2.split()
+
+    list_numbers2 = []
+
+    for num in numbers2:
+        x2 = int(num)
+        list_numbers2.append(x2)
+
+    # Call out the bloody function
+    answer = appendList(list_numbers, list_numbers2)
+    print(answer)
+
+if __name__ == "__main__":
+    main()
