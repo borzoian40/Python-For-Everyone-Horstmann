314cba6409c9f4393f25a3a4fa6f8ee7d6f48abb
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-11-16 14:21:44 +0100

    Merge Append

---
+++ Chapter 6/P6.30.py
@@ -0,0 +1,10 @@
+"""
+P6.30 Write a function def merge(a, b) that merges two lists, alternating elements from both 
+lists. If one list is shorter than the other, then alternate as long as you can and then 
+append the remaining elements from the longer list. For example, if a is
+1 4 9 16
+and b is
+9 7 4 9 11
+then merge returns a new list containing the values
+1 9 4 7 9 4 16 9 11
+"""
