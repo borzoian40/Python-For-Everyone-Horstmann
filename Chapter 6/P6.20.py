3ca4f2c0142502e570b9a78438f4b95602062ef1
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-11-18 09:48:50 +0100

    P6.20.py

--- Chapter 6/P6.20.py
+++ Chapter 6/P6.20.py
@@ -10,3 +10,44 @@ In your program, produce a random starting configuration and print it. Then keep
 applying the solitaire step and print the result. Stop when the solitaire final configuration is reached.
 
 """
+import random
+
+def generate_random_configuration(total_cards):
+    piles = []
+    remaining_cards = total_cards
+
+    while remaining_cards > 0:
+        pile_size = random.randint(1, remaining_cards)
+        piles.append(pile_size)
+        remaining_cards -= pile_size
+
+    return piles
+
+def solitaire_step(piles):
+    new_piles = [pile - 1 for pile in piles if pile > 1]
+    new_piles.append(len(piles))
+    return new_piles
+
+def is_final_configuration(piles):
+    return sorted(piles) == list(range(1, 10))
+
+def print_configuration(piles):
+    print(" ".join(str(pile) for pile in piles))
+
+def main():
+    # Generate a random starting configuration
+    initial_configuration = generate_random_configuration(45)
+
+    # Print the initial configuration
+    print("Initial Configuration:")
+    print_configuration(initial_configuration)
+
+    # Apply the solitaire step until the final configuration is reached
+    current_configuration = initial_configuration
+    while not is_final_configuration(current_configuration):
+        current_configuration = solitaire_step(current_configuration)
+        print("Next Configuration:")
+        print_configuration(current_configuration)
+
+if __name__ == "__main__":
+    main()
