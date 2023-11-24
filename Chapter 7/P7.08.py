5031a480f935fc3b8062e9bccd4ddbe15da2c28c
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-11-24 16:22:39 +0100

    P7.08.py

---
+++ Chapter 7/P7.08.py
@@ -0,0 +1,8 @@
+"""
+•• P7.8 Write a program that replaces each line of a file with its reverse. For example, if you run
+python reverse.py hello.py
+then the contents of hello.py are changed to
+.margorp nohtyP tsrif yM #
+)"!dlroW ,olleH"(tnirp
+Of course, if you run Reverse twice on the same file, you get back the original file
+"""
